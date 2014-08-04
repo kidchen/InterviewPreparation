803f301a7e6e8117fa9fbe9e4c4354e1d7999bab
zhch1990 <chenzhang2012@live.cn>
2014-08-04 12:48:20 -0400

    Update and rename PalindromeNumber to PalindromeNumber.java
    Determine whether an integer is a palindrome. Do this without extra space.
    
    click to show spoilers.
    
    Some hints:
    Could negative integers be palindromes? (ie, -1)
    
    If you are thinking of converting the integer to string, note the restriction of using extra space.
    
    You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
    
    There is a more generic way of solving this problem.

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -19,3 +19,22 @@ public class Solution {
         return true;
     }
 }
+
+
+// general solution:
+public boolean isPalindrome(int x) {
+	if(x < 0) {
+		return false;
+	}
+	String str = new String();
+	str = String.valueOf(x);
+	int left = 0, right = str.length() - 1;
+	while(left <= right) {
+		if(str.charAt(left) != str.charAt(right)) {
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
+}
