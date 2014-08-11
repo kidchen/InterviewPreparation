a00de9d6b8cecb09ad180a6026f5b15081d15ff6
zhch1990 <chenzhang2012@live.cn>
2014-08-11 13:39:51 -0400

    Update and rename ConvertSortedArraytoBinarySearchTree to ConvertSortedArraytoBinarySearchTree.java

--- ConvertSortedArraytoBinarySearchTree.java
+++ ConvertSortedArraytoBinarySearchTree.java
@@ -1,3 +1,7 @@
+/*
+*  Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -7,15 +11,16 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
 public class Solution {
     public TreeNode sortedArrayToBST(int[] num) {
         if(num.length==0) return null;
         return sortedArrayToBST(num, 0, num.length-1);
     }
     
-    TreeNode sortedArrayToBST(int[] num, int start, int end){
+    private TreeNode sortedArrayToBST(int[] num, int start, int end) {
         if(start > end) return null;
-        int mid = (start+end)/2;
+        int mid = (start + end) / 2;
         TreeNode root = new TreeNode(num[mid]);
         root.left = sortedArrayToBST(num, start, mid-1);
         root.right = sortedArrayToBST(num, mid+1, end);
