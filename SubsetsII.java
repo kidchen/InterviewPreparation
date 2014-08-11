b56c20ad1eccbf7827649806e3054986a95ab6bf
zhch1990 <chenzhang2012@live.cn>
2014-08-10 22:27:01 -0400

    Update and rename SubsetsII to SubsetsII.java

--- SubsetsII.java
+++ SubsetsII.java
@@ -1,17 +1,37 @@
+/*
+Given a collection of integers that might contain duplicates, S, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+*/
+
 public class Solution {
     public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> subset = new ArrayList<Integer>();
-        // don't forget to sort
+        // !!! don't forget to sort !!!
         Arrays.sort(num);
         helper(num, result, subset, 0);
         return result;
     }
     
     void helper(int[] num, ArrayList<ArrayList<Integer>> result, ArrayList<Integer> subset, int len){
-        // have to use new ArrayList, because subset will be changed later
+        // !!! have to use new ArrayList, because subset will be changed later !!!
         result.add(new ArrayList<Integer>(subset));
         for(int i=len; i<num.length; i++){
+            // skip duplicates
             if(i!=len && num[i]==num[i-1]){
                 continue;
             }
