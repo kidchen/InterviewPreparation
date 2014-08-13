912d672e9dd3c0cb666b3fecd0424589ee7a3cbd
zhch1990 <chenzhang2012@live.cn>
2014-08-13 17:35:40 -0400

    Update and rename BinaryTreeLevelOrderTraversalII to BinaryTreeLevelOrderTraversalII.java

--- BinaryTreeLevelOrderTraversalII.java
+++ BinaryTreeLevelOrderTraversalII.java
@@ -1,3 +1,22 @@
+/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+*/
+
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -12,7 +31,7 @@ public class Solution {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         if(root==null) return result;
         helper(root,result,0);
-        Collections.reverse(result);                     // !!! only need to add this line
+        Collections.reverse(result);                     // !!! only need to add this line !!!
         return result;
     }
     
