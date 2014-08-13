b3fb89a030e02cdd669e35e0d9c51f056ecff204
zhch1990 <chenzhang2012@live.cn>
2014-08-13 18:20:46 -0400

    Update Construct Binary Tree from Preorder and Inorder Traversal.java

--- Construct Binary Tree from Preorder and Inorder Traversal.java
+++ Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -9,6 +9,9 @@
  */
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // Preorder: 12453687. Inorder: 42516837
+        // Preorder: Left: 245, Right: 3687, Root: 1 (by index)
+        // Inorder: Left: 425, Right: 6837, Root: 1
         int preEnd = preorder.length - 1;
         int inEnd = inorder.length - 1;
         return buildTree(preorder, inorder, 0, preEnd, 0, inEnd);
@@ -16,17 +19,27 @@ public class Solution {
     
     TreeNode buildTree(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd){
         if(preStart > preEnd || inStart > inEnd) return null;
-        // find root
+        // find root:
+        // The root is the first element in Preorder.
         int rootVal = preorder[preStart];
         TreeNode root = new TreeNode(rootVal);
-        // find left & right
-        int devide = 0;
-        for(devide = 0; devide < inorder.length; devide++){
-            if(inorder[devide] == rootVal) break;
+        // find left & right:
+        // In the inorder, find the root and it will divide all nodes into two.
+        int divide = 0;
+        for(divide = 0; divide < inorder.length; devide++){
+            if(inorder[divide] == rootVal) break;
         }
-        // DFS
-        root.left = buildTree(preorder, inorder, preStart + 1, devide + preStart - inStart, inStart, devide - 1);
-        root.right = buildTree(preorder, inorder, preStart - inStart + devide + 1, preEnd, devide + 1, inEnd);
+        // DFS:
+        // In left part: preStart = preStart + 1
+        //      !!!      preEnd = divide + prestart - inStart, we need to -inStart because ?
+        //               inStart = inStart
+        //               inEnd = divide - 1
+        root.left = buildTree(preorder, inorder, preStart + 1, divide + preStart - inStart, inStart, divide - 1);
+        //      !!!       preStart = preStart + divide + 1 - inStart, nee to -inStart because ?
+        // In right part: preEnd = preEnd
+        //                inStart = divide + 1
+        //                inEnd = inEnd
+        root.right = buildTree(preorder, inorder, preStart + divide + 1 - inStart, preEnd, divide + 1, inEnd);
         return root;
     }
 }
