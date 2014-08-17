cce262d73f5db272be261c5464f29aa6b0e275bf
zhch1990 <chenzhang2012@live.cn>
2014-08-16 21:22:08 -0400

    Update SortArrayByFrequency.java

--- SortArrayByFrequency.java
+++ SortArrayByFrequency.java
@@ -12,6 +12,7 @@ public class SortArrayByFrequency {
 		
 	static int[] sortArray(int[] unsortedArray) {
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		// build the map: array elements as keys, their show times as values
 		for(int i : unsortedArray) {
 			if(map.containsKey(i)) {
 				int count = map.get(i);
@@ -21,17 +22,24 @@ public class SortArrayByFrequency {
 				map.put(i, 1);
 			}
 		}
+		// maxCount: largest value in the map
 		int maxCount = 0;
+		// element: most frequently element in the array
 		int element = 0;
+		// new array index
 		int start = 0;
 		int[] result = new int[unsortedArray.length];
 		while(!map.isEmpty()) {
+			// find the most frequent element in the map
 			for(int i : map.keySet()) {
 				if(map.get(i) > maxCount) {
 					maxCount = map.get(i);
 					element = i;
 				}
 			}
+			// start: result array's index
+			// each time, add maxCount times elements
+			// Noted that i = start
 			for(int i = start; i < start + maxCount; i++) {
 				result[i] = element;
 			}
