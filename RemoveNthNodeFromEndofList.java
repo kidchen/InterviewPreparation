55cad13a6be0bb85525c136bcf9399a1d29284ff
zhch1990 <chenzhang2012@live.cn>
2014-08-16 20:53:13 -0400

    Update and rename RemoveNthNodeFromEndofList to RemoveNthNodeFromEndofList.java

---
+++ RemoveNthNodeFromEndofList.java
@@ -0,0 +1,90 @@
+/*
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+   Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Try to do this in one pass.
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head==null) return head;
+        if(head.next==null) return null;
+        ListNode slow=head, fast=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        if(fast==null){
+            head=head.next;
+            return head;
+        }
+        while(fast.next!=null){              // fast.next != null !!! not fast!=null
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
+
+/***********UPDATE AUG 16*************/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null) {
+            return head;
+        }
+        ListNode fast = head;
+        int i = 0;
+        // !!! fast, not fast.next != null !!!
+        // keep in mind that if only one element in the list
+        while(fast != null && i < n) {
+            fast = fast.next;
+            i++;
+        }
+        // if n is invalid (bigger than the number of list nodes)
+        if(i < n) {
+            return head;
+        }
+        // if n equals to the number of list nodes (that is, we remove the head)
+        if(fast == null) {
+            return head.next;
+        }
+        // general cases
+        ListNode slow = head;
+        while(fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
