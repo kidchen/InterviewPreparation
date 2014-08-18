4d5ab33898e280cbaeae64e0f5e9722d57d3d07e
zhch1990 <chenzhang2012@live.cn>
2014-08-17 21:57:42 -0400

    Update SwapNodesinPairs.java

--- SwapNodesinPairs.java
+++ SwapNodesinPairs.java
@@ -28,3 +28,41 @@ public class Solution {
 
 // general
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode result = new ListNode(0);
+        result.next = head;
+        ListNode pre = result;
+        ListNode cur = head;
+        while(cur != null && cur.next != null) {
+            // 1-2-3-4 --> 2-1-4-3
+            ListNode next = cur.next.next;
+            // !!! 2.next = 1 !!!
+            cur.next.next = cur;
+            // 2-1-...
+            pre.next = cur.next;
+            if(next != null && next.next != null) {
+                cur.next = next.next;
+            } else {
+                cur.next = next;
+            }
+            pre = cur;
+            cur = next;
+        }
+        return result.next;
+    }
+}
