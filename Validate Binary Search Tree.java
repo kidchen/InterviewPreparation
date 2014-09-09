a835d15f93c6cc5b3cc50e637e697f07d4b914b8
Chen Zhang <chenzhang2012@live.cn>
2014-09-09 00:50:03 -0700

    Update Validate Binary Search Tree.java

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -20,3 +20,72 @@ public class Solution {
         return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
     }
 }
+
+
+// by inorder traversal (recursion)
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer> pre = new ArrayList<Integer>();
+        pre.add(null);
+        return helper(root, pre);
+    }
+    
+    boolean helper(TreeNode root, ArrayList<Integer> pre) {
+        if(root == null) {
+            return true;
+        }
+        boolean left = helper(root.left, pre);
+        // !!! how about root.val == pre.get(0) : false !!!
+        if(pre.get(0) != null && root.val <= pre.get(0)) {
+            return false;
+        }
+        pre.set(0, root.val);
+        return left && helper(root.right, pre);
+    }
+}
+
+
+// by inorder traversal (iteration)
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode pre = null;
+        while(root != null || !stack.isEmpty()) {
+            if(root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                if(pre != null && pre.val >= root.val) {
+                    return false;
+                }
+                pre = root;
+                root = root.right;
+            }
+        }
+        return true;
+    }
+}
