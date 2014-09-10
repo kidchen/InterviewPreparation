7178c50c4a49cd6c2279ec8f5a5187a2a6d55f94
Chen Zhang <chenzhang2012@live.cn>
2014-09-10 16:43:47 -0700

    Update and rename BalancedBinaryTree to BalancedBinaryTree.java

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -25,3 +25,36 @@ public class Solution {
         return Math.max(leftHeight, rightHeight) + 1;
     }
 }
+
+
+// DFS. O(n), O(logn), recursion:
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return helper(root) >= 0;
+    }
+    
+    int helper(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if(left < 0 || right < 0) {
+            return -1;
+        }
+        if(Math.abs(left - right) >= 2) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
