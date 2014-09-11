a3f056fb0b69843c79687a1d5c804a93c1435006
Chen Zhang <chenzhang2012@live.cn>
2014-09-10 17:24:20 -0700

    Update and rename LinkedListCycle to LinkedListCycle.java

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -11,10 +11,11 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) return false;
+        if (head == null || head.next == null) 
+            return false;
         ListNode slow = head;
         ListNode fast = head.next;
-        while(fast != null && fast.next != null){
+        while(fast != null && fast.next != null) {
             if (slow == fast) return true;
             slow = slow.next;
             fast = fast.next.next;
