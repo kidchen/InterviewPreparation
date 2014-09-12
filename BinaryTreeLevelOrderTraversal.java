14b2d5cc590f9bffd92b7547a7f2f708531db287
Chen Zhang <chenzhang2012@live.cn>
2014-09-12 14:36:59 -0700

    Update BinaryTreeLevelOrderTraversal.java

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -28,3 +28,42 @@ public class Solution {
         helper(root.right,result,level+1);
     }
 }
+
+
+
+// BFS:
+
+public void levelOrder(TreeNode root) {
+    if(root == null)
+        return;
+    // use a queue to store the parent nodes
+    LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.add(root);
+    // store the current level's nodes number
+    int currentNum = 0;
+    // store the last level's nodes number
+    int lastNum = 1;
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    while(!queue.isEmpty()) {
+        TreeNode cur = queue.poll();
+        lastNum--;
+        list.add(cur.val);
+        if(cur.left!=null) {
+            queue.add(cur.left);
+            currentNum++;
+        }
+        if(cur.right!=null) {
+            queue.add(cur.right);
+            currentNum++;
+        }
+        if(lastNum==0) {
+            lastNum = currentNum;
+            currentNum = 0;
+            for(int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+            list = new ArrayList<Integer>();
+        }
+    }
+    return;
+}
