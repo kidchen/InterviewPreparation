0ecf6e338bab3ced6afe2fa81bb30f47ea84a90a
Chen Zhang <chenzhang2012@live.cn>
2014-09-14 04:19:22 -0700

    Update Median of Two Sorted Arrays.java

--- Median of Two Sorted Arrays.java
+++ Median of Two Sorted Arrays.java
@@ -1,4 +1,6 @@
-There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+/* There are two sorted arrays A and B of size m and n respectively. 
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ */
 
 
 public class Solution {
