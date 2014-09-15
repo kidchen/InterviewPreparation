1990bd47345d9d4639bb3d96139ce285647a88f4
Chen Zhang <chenzhang2012@live.cn>
2014-09-14 20:31:26 -0700

    Update ThreeSum.java

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,3 +1,19 @@
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+The solution set must not contain duplicate triplets.
+    For example, given array S = {-1 0 1 2 -1 -4},
+
+    A solution set is:
+    (-1, 0, 1)
+    (-1, -1, 2)
+*/
+
+// O(nlogn + n^2) = O(n^2), space O(n)
+
+
 public class Solution {
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
