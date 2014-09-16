aeb234ec48cfb0c817e2ce545158b0c1e28e6033
Chen Zhang <chenzhang2012@live.cn>
2014-09-16 00:12:51 -0700

    Update Permutation Sequence.java

--- Permutation Sequence.java
+++ Permutation Sequence.java
@@ -1,8 +1,26 @@
+/*
+The set [1,2,3,…,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order,
+We get the following sequence (ie, for n = 3):
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Note: Given n will be between 1 and 9 inclusive.
+*/
+
+// O(n^2), O(1) space
+
+
 public class Solution {
     public String getPermutation(int n, int k) {
         StringBuilder result = new StringBuilder();
-        // we need to use (n-1)!, so k--
-        k--;
         // calculate (n-1)!
         int fact = 1;
         for(int i = 2; i < n; i++) {
@@ -13,14 +31,20 @@ public class Solution {
         for(int i = 1; i <= n; i++) {
             num.add(i);
         }
-        // calculate each position
+        // !!! round is the length of the number, zero-based !!!
         int round = n - 1;
+        // !!! make k to zero-based !!!
+        k--;
         while(round >= 0) {
+            // every (n-1)! set, the current digit will be changed to the next in num
             int index = k / fact;
-            // !!!
-            k %= fact;
+            // add current digit to result
             result.append(num.get(index));
+            // !!! remove the element, for the next while loop !!!
             num.remove(index);
+            // !!! find out the new kth in (n-1)! !!!
+            k %= fact;
+            // !!! if it is not the last element, recalculate (n-1)! by minus n by one !!!
             if(round > 0) {
                 fact /= round;
             }
@@ -29,3 +53,5 @@ public class Solution {
         return result.toString();
     }
 }
+
+// More related with math -- not easy to think in a short time, remember the pattern.
