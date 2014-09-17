cf1176c52752f5e260a77b9b683db1c1154254dc
Chen Zhang <chenzhang2012@live.cn>
2014-09-16 23:48:22 -0700

    Update Gas Station.java

--- Gas Station.java
+++ Gas Station.java
@@ -1,3 +1,45 @@
+/*
+There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+
+Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+Note:
+The solution is guaranteed to be unique.
+*/
+
+// O(n), O(1) space
+// key point: if the sum of gas from start to current position is negative, any position between them is not suitable either !!!
+
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if(gas == null || gas.length == 0 || cost == null || cost.length == 0 || gas.length != cost.length) {
+            return -1;
+        }
+        int leftInTank = 0;
+        int start = 0;
+        int total = 0;
+        for(int i = 0; i < gas.length; i++) {
+            // how many gas left after moving to position i
+            int remain = gas[i] - cost[i];
+            leftInTank += remain;
+            // if negative (cannot move to place i)
+            if(leftInTank < 0) {
+                leftInTank = 0;
+                // this place is not good to start, move to the next
+                start = i + 1;
+            }
+            // calculate the total gas remain
+            total += remain;
+        }
+        return total >= 0 ? start : -1;
+    }
+}
+
+
+/******** old solution *********/
+
 public class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         // DP Problem
