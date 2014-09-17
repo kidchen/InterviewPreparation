945c0eb61c01912188c93f40a770104a190b63b7
Chen Zhang <chenzhang2012@live.cn>
2014-09-16 20:24:58 -0700

    Update ClimbingStair.java

--- ClimbingStair.java
+++ ClimbingStair.java
@@ -1,5 +1,5 @@
-//You are climbing a stair case. It takes n steps to reach to the top.
-//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// You are climbing a stair case. It takes n steps to reach to the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 
 public class Solution {
@@ -21,5 +21,31 @@ public class Solution {
         }
         // or return oneStep (oneStep = temp) and we don't need to initialize temp
         return temp;
+    }
+}
+
+
+// Fibonacci, O(n)
+// Note: Fibonacci actually has O(logn) solution in math method
+
+public class Solution {
+    public int climbStairs(int n) {
+        if(n < 1) {
+            return 0;
+        }
+        int f1 = 1;
+        int f2 = 2;
+        if(n == 1) {
+            return f1;
+        }
+        if(n == 2) {
+            return f2;
+        }
+        for(int i = 2; i < n; i++) {
+            int f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f2;
     }
 }
