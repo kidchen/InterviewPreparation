bd8ad82840394843e484db0ff3f504a580cb1bd7
Chen Zhang <chenzhang2012@live.cn>
2014-09-18 15:54:53 -0700

    Update Reverse Nodes in k-Group.java

--- Reverse Nodes in k-Group.java
+++ Reverse Nodes in k-Group.java
@@ -1,3 +1,23 @@
+/*
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+*/
+
+// O(n), O(1) space
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,6 +31,9 @@
  */
 public class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null) {
+            return head;
+        }
         // dummy-->head(1->2->3->4) ,k = 3
         // (pre) -> 1(cur) -> 2(next) -> 3 -> (4 end)
         ListNode dummy = new ListNode(0);
@@ -18,18 +41,16 @@ public class Solution {
         ListNode pre = dummy;
         // !!! cur = head. because we will return dummy.next, not head !!!
         ListNode cur = head;
-        // !!! DO NOT add head.next == null !!!
-        if(head == null) return null;
-        int count = 0;
+        int count = 1;
         while(cur != null) {
-            count++;
             ListNode next = cur.next;
             if(count == k) {
-                // pre will be moved forward
+                // !!! the reverse is actually in place, we only need to move pre to its right position !!!
+                // !!! pre will be moved forward !!! pre = , rather than pre.next = ; pre = cur !!!
                 pre = reverse(pre, next);
-                // !!! clear count !!!
                 count = 0;
             }
+            count++;
             // !!! cur = next, rather than cur = cur.next !!!
             cur = next;
         }
