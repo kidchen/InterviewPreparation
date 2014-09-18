d4d8ebc6120a0b5c0673152c09cdd51c30a9c4ca
Chen Zhang <chenzhang2012@live.cn>
2014-09-17 18:58:42 -0700

    Update PartitionList.java

--- PartitionList.java
+++ PartitionList.java
@@ -1,3 +1,15 @@
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+*/
+
+// O(n), O(1) space
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,30 +23,34 @@
  */
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head == null) return null;
-        // !!! don't forget to initial the value "0" !!!
-        ListNode small = new ListNode(0);
-        ListNode large = new ListNode(0);
-        ListNode sHead = small;
-        ListNode lHead = large;
-        // !!! not head.next == null, but head !!!
-        while(head != null) {
-            if(head.val < x){
-                small.next = head;
-                small = small.next;
-            } else {
-                large.next = head;
-                large = large.next;
-            }
-            // !!! don't forget to move head !!!
-            head = head.next;
-            // !!! have to close the listnode !!!
-            // otherwise there may create a linkedlist cycle
-            small.next = null;
-            large.next = null;
+        if(head == null || head.next == null) {
+            return head;
         }
-        // !!! equals to lHead.next, not lHead (both Head equals to 0) !!!
-        small.next = lHead.next;
-        return sHead.next;
+        ListNode dummy = new ListNode(0);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        ListNode last = new ListNode(0);
+        ListNode lastCur = last;
+        while(cur != null) {
+            ListNode next = cur.next;
+            if(cur.val < x) {
+                pre.next = cur;
+                pre = pre.next;
+                // !!! have to close the listnode !!!
+                // !!! otherwise there may create a linkedlist cycle !!!
+                pre.next = null;
+            } else {
+                lastCur.next = cur;
+                lastCur = lastCur.next;
+                // !!! have to close the listnode !!!
+                // !!! otherwise there may create a linkedlist cycle !!!
+                lastCur.next = null;
+            }
+            cur = next;
+        }
+        if(last.next != null) {
+            pre.next = last.next;
+        }
+        return dummy.next;
     }
 }
