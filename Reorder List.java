041ccb2c1297e260c3e79eb12ef1f17d8163934c
Chen Zhang <chenzhang2012@live.cn>
2014-09-18 23:01:08 -0700

    Update Reorder List.java

--- Reorder List.java
+++ Reorder List.java
@@ -1,3 +1,16 @@
+/*
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You must do this in-place without altering the nodes' values.
+
+For example,
+Given {1,2,3,4}, reorder it to {1,4,2,3}.
+*/
+
+// O(n), O(1) space
+
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -11,40 +24,39 @@
  */
 public class Solution {
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null) return;
-        ListNode fast = head;
+        if(head == null || head.next == null || head.next.next == null) {
+            return;
+        }
+        // divide
         ListNode slow = head;
-        // !!! order matters !!!
-        while(fast.next != null && fast.next.next != null) {
+        ListNode fast = head;
+        // !!! don't forget the fast.next != null condition !!!
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         ListNode head1 = head;
-        ListNode head2 = slow.next;
+        ListNode newHead = slow.next;
         slow.next = null;
-        head2 = reverse(head2);
-        while(head1 != null && head2 != null) {
-            ListNode next = head2.next;
-            head2.next = head1.next;
-            head1.next = head2;
-            head1 = head2.next;
-            head2 = next;
-        }
-    }
-    
-    ListNode reverse(ListNode head) {
+        // !!! reverse head2 !!!
+        // DON'T NEED TO USE dummy NODE
         ListNode pre = null;
-        ListNode cur = head;
+        ListNode cur = newHead;
         while(cur != null) {
-            // 1(cur)->2(next)->3->4
             ListNode next = cur.next;
-            // 1(cur)->null(pre) 2(next)->3->4
             cur.next = pre;
-            // 1(pre)->null 2(next)->3->4
             pre = cur;
-            // 1(pre)->null 2(cur)->3->4
             cur = next;
         }
-        return pre;
+        ListNode head2 = pre;
+        // merge
+        while(head1 != null && head2 != null) {
+            ListNode temp = head2.next;
+            head2.next = head1.next;
+            head1.next = head2;
+            head1 = head1.next.next;
+            head2 = temp;
+        }
+        return;
     }
 }
