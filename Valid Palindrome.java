57a4396b7201d9ca67b4773f35ac7388689c1ab0
Chen Zhang <chenzhang2012@live.cn>
2014-09-19 15:13:36 -0700

    Update Valid Palindrome.java

--- Valid Palindrome.java
+++ Valid Palindrome.java
@@ -1,41 +1,57 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+*/
+
+// O(n), O(1) space
+
 public class Solution {
     public boolean isPalindrome(String s) {
-        if(s == null || s.length() == 0) return true;
-        int left = 0;
-        int right = s.length() - 1;
-        while(left < right) {
-            if(!isValid(s.charAt(left))) {
-                left++;
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        int start = 0;
+        int end = s.length() - 1;
+        while(start <= end) {
+            char left = s.charAt(start);
+            char right = s.charAt(end);
+            if(!valid(left)) {
+                start++;
                 continue;
             }
-            if(!isValid(s.charAt(right))) {
-                right--;
+            if(!valid(right)) {
+                end--;
                 continue;
             }
-            if(!isSame(s.charAt(left), s.charAt(right))) {
+            // A --> a: +32
+            if(left >= 'A' && left <= 'Z') {
+                left += 32;
+            }
+            if(right >= 'A' && right <= 'Z') {
+                right += 32;
+            }
+            if(left != right) {
                 return false;
+            } else {
+                start++;
+                end--;
             }
-            left++;
-            right--;
         }
         return true;
     }
     
-    boolean isValid(char c) {
-        if(c >= '0' && c <='9' || c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
+    private boolean valid(char c) {
+        if(c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
             return true;
         }
         return false;
-    }
-    
-    boolean isSame(char c1, char c2) {
-        if(c1 >= 'A' && c1 <= 'Z') {
-            // !!! convert !!!
-            c1 = (char)(c1 + 'a' - 'A');
-        }
-        if(c2 >= 'A' && c2 <= 'Z') {
-            c2 = (char)(c2 + 'a' - 'A');
-        }
-        return c1 == c2;
     }
 }
