89a494b3a561d47433fb71a4a275bef15fb0d223
Chen Zhang <chenzhang2012@live.cn>
2014-09-20 00:50:56 -0700

    Update Add Binary.java

--- Add Binary.java
+++ Add Binary.java
@@ -1,3 +1,75 @@
+/*
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+*/
+
+// O(m+n), O(max(m,n)) space cost for storing result
+
+public class Solution {
+    public String addBinary(String a, String b) {
+        if(a == null || a.length() == 0) {
+            return b;
+        }
+        if(b == null || b.length() == 0) {
+            return a;
+        }
+        int aend = a.length() - 1;
+        int bend = b.length() - 1;
+        // !!! DON'T use count++/count--, since count could only be 0 or 1 !!!
+        int carry = 0;
+        StringBuffer result = new StringBuffer();
+        while(aend >= 0 && bend >= 0) {
+            int aint = (int)(a.charAt(aend) - '0');
+            int bint = (int)(b.charAt(bend) - '0');
+            int sum = aint + bint + carry;
+            if(sum >= 2) {
+                sum -= 2;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            result.append(sum);
+            aend--;
+            bend--;
+        }
+        // careful about the code, while() rather than if()...
+        while(aend >= 0) {
+            int sum = (int)(a.charAt(aend) - '0') + carry;
+            if(sum >= 2) {
+                sum -= 2;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            result.append(sum);
+            aend--;
+        }
+        while(bend >= 0) {
+            int sum = (int)(b.charAt(bend) - '0') + carry;
+            if(sum >= 2) {
+                sum -= 2;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            result.append(sum);
+            bend--;
+        }
+        if(carry > 0) {
+            result.append(1);
+        }
+        return result.reverse().toString();
+    }
+}
+
+
+
+/******** OLD VERSION *********/
+
 public class Solution {
     public String addBinary(String a, String b) {
         // length of a and b
