060f79315d05b3a848970ea1f96341bdf37c1f1e
Chen Zhang <chenzhang2012@live.cn>
2014-09-22 03:18:24 -0700

    Update BinaryTreeZigzagLevelOrderTraversal.java

--- BinaryTreeZigzagLevelOrderTraversal.java
+++ BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,3 +1,75 @@
+/*
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+*/
+
+// iteration(BFS): use Collections.reverse(), this operation is linear time cost of time
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(root == null) {
+            return result;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        ArrayList<Integer> level = new ArrayList<Integer>();
+        // !!! don't forget to add root at first !!!
+        queue.add(root);
+        int levelNum = 0;
+        int lastNum = 1;
+        int curNum = 0;
+        while(!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            level.add(cur.val);
+            lastNum--;
+            if(cur.left != null) {
+                queue.add(cur.left);
+                curNum++;
+            }
+            if(cur.right != null) {
+                queue.add(cur.right);
+                curNum++;
+            }
+            if(lastNum == 0) {
+                lastNum = curNum;
+                curNum = 0;
+                levelNum++;
+                if(levelNum % 2 == 0){
+                    Collections.reverse(level);
+                }
+                result.add(level);
+                level = new ArrayList<Integer>();
+            }
+        }
+        return result;
+    }
+}
+
+
+// recursion, DFS
+
 /**
  * Definition for binary tree
  * public class TreeNode {
