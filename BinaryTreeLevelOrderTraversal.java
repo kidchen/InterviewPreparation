b862626c40c69ebe6cfcb15a292a4008819ef397
Chen Zhang <chenzhang2012@live.cn>
2014-09-22 02:59:47 -0700

    Update BinaryTreeLevelOrderTraversal.java

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -1,3 +1,25 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+*/
+
+// O(n), O(n) space cost
+
+// DFS: recursion, similar with preorder traversal
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -10,60 +32,70 @@
 public class Solution {
     public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        if(root==null) return result;
+        if(root == null) {
+            return result;
+        }
         helper(root, result, 1);
         return result;
     }
     
-    void helper(TreeNode root, ArrayList<ArrayList<Integer>> result, int level){
-        // root.left and root.right are null
-        if(root == null) return;
-        // new level
-        if(level>result.size()){
+    private void helper(TreeNode root, ArrayList<ArrayList<Integer>> result, int level) {
+        if(root == null) {
+            return;
+        }
+        if(level > result.size()) {
             result.add(new ArrayList<Integer>());
         }
-        // add this node value (level is start from 1, so we need to "-1")
-        result.get(level-1).add(root.val);
-        helper(root.left,result,level+1);
-        helper(root.right,result,level+1);
+        result.get(level - 1).add(root.val);
+        helper(root.left, result, level + 1);
+        helper(root.right, result, level + 1);
     }
 }
 
 
 
-// BFS:
+// BFS: iteration level by level, using a queue to store the last level's nodes
 
-public void levelOrder(TreeNode root) {
-    if(root == null)
-        return;
-    // use a queue to store the parent nodes
-    LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
-    queue.add(root);
-    // store the current level's nodes number
-    int currentNum = 0;
-    // store the last level's nodes number
-    int lastNum = 1;
-    ArrayList<Integer> list = new ArrayList<Integer>();
-    while(!queue.isEmpty()) {
-        TreeNode cur = queue.poll();
-        lastNum--;
-        list.add(cur.val);
-        if(cur.left!=null) {
-            queue.add(cur.left);
-            currentNum++;
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(root == null) {
+            return result;
         }
-        if(cur.right!=null) {
-            queue.add(cur.right);
-            currentNum++;
-        }
-        if(lastNum==0) {
-            lastNum = currentNum;
-            currentNum = 0;
-            for(int i = 0; i < list.size(); i++) {
-                System.out.println(list.get(i));
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        ArrayList<Integer> level = new ArrayList<Integer>();
+        queue.add(root);
+        int lastNum = 1;
+        int curNum = 0;
+        while(!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            level.add(cur.val);
+            lastNum--;
+            if(cur.left != null) {
+                queue.add(cur.left);
+                curNum++;
             }
-            list = new ArrayList<Integer>();
+            if(cur.right != null) {
+                queue.add(cur.right);
+                curNum++;
+            }
+            if(lastNum == 0) {
+                lastNum = curNum;
+                curNum = 0;
+                result.add(level);
+                // can't use clear(), since we need to use extra space to store the result anyway
+                level = new ArrayList<Integer>();
+            }
         }
+        return result;
     }
-    return;
 }
