fc0ef8e5592ef4ad7b0243e04b84c7db27dee5f0
Chen Zhang <chenzhang2012@live.cn>
2014-09-23 21:39:28 -0700

    Update Jump Game II.java

--- Jump Game II.java
+++ Jump Game II.java
@@ -1,9 +1,16 @@
 public class Solution {
     public int jump(int[] A) {
-        if(A == null || A.length == 0) return 0;
+        if(A == null || A.length == 0) {
+            return 0;
+        }
+        /*
+        maxPath: so far, the max path(index) we can reach
+        lastMaxPath: when current path(index) is bigger than lastmaxPath, step++, set the lastMaxPath as maxPath
+        step: calculate how many steps should we take
+        */
         int maxPath = 0, lastMaxPath = 0, step = 0;
-        // !!! add condition " i <= maxPath" !!!
-        for(int i =0; i < A.length && i <= maxPath; i++) {
+        // !!! add condition " i <= maxPath", so that we can break when there is unreachable to the last !!!
+        for(int i = 0; i < A.length && i <= maxPath; i++) {
             // current position is beyond the last jump: add step and jump to the next maxPath
             if(i > lastMaxPath) {
                 step++;
@@ -12,9 +19,10 @@ public class Solution {
             // calculate the maxPath in current position
             maxPath = Math.max(A[i] + i, maxPath);
         }
-        // !!! REMEMBER TO "- 1" because we use index above to calculate path and 
-        // now we need to -1 to check whether we can reach the last element
-        if(maxPath < A.length - 1) return 0;
+        // check the availability
+        if(maxPath < A.length - 1) {
+            return 0;
+        }
         return step;
     }
 }
