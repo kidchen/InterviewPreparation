bfdda5d2b0f905f560ab8e958fb58b1d4f91bfb3
Chen Zhang <chenzhang2012@live.cn>
2014-10-06 16:27:18 -0700

    Update Substring with Concatenation of All Words.java

--- Substring with Concatenation of All Words.java
+++ Substring with Concatenation of All Words.java
@@ -1,5 +1,7 @@
 /*
-You are given a string, S, and a list of words, L, that are all of the same length. Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+You are given a string, S, and a list of words, L, that are all of the same length. 
+Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once 
+and without any intervening characters.
 
 For example, given:
 S: "barfoothefoobarman"
