09dd89576127fd65bf10946f74df23e7a010b54c
Chen Zhang <chenzhang2012@live.cn>
2014-10-08 04:14:26 -0700

    Update Word Break.java

--- Word Break.java
+++ Word Break.java
@@ -1,18 +1,36 @@
+/*
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+*/
+
+// DP: O(n^2), O(n)
+
 public class Solution {
     public boolean wordBreak(String s, Set<String> dict) {
-        if(s == null || s.length() == 0) return true;
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        // store whether all chars can form words from dict *so far*
         boolean[] result = new boolean[s.length() + 1];
+        // record the result from result[1]
         result[0] = true;
-        // !!! from i = 0 !!!
+        // have to do from 0, eg: "a", ["a"]
         for(int i = 0; i < s.length(); i++) {
-            // !!! have to use stringBuilder for later delete use !!!
-            StringBuilder word = new StringBuilder(s.substring(0, i + 1));
+            // !!! from 0 each time !!!
+            StringBuffer word = new StringBuffer(s.substring(0, i + 1));
+            // j is the left boundary of the substring
             for(int j = 0; j <= i; j++) {
+                // if all chars can form words from dict until j, and substring(j,i+1) contains in dict:
                 if(result[j] && dict.contains(word.toString())) {
                     result[i + 1] = true;
                     break;
                 }
-                // if the word doesn't match dict, delete the first char(tcode --> code)
+                // if not, delete the left most char in the substring
                 word.deleteCharAt(0);
             }
         }
