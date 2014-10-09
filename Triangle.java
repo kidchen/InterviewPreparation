7857d3226bbcf4df8c7fb057ea9843007d44de84
Chen Zhang <chenzhang2012@live.cn>
2014-10-08 22:23:45 -0700

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,3 +1,48 @@
+/*
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Note:
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+*/
+
+// O(n), O(n) space
+
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() == 0) {
+            return 0;
+        }
+        // !!! don't forget add 1 !!!
+        int num[] = new int[triangle.size() + 1];
+        // from bottom to up
+        for (int row = triangle.size() - 1; row >= 0; row--) {
+            List<Integer> level = triangle.get(row);
+            // !!! do the for loop including col = row
+            for (int col = 0; col <= row; col++) {
+                num[col] = Math.min(level.get(col) + num[col], level.get(col) + num[col + 1]);
+            }
+        }
+        return num[0];
+    }
+}
+
+
+
+
+
+
+/********* Yodle **********/
+
+
 package interview;
 
 import java.io.File;
