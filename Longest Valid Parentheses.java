7b7eefcdd81e8f3a0df4df6687e519c07b01463d
Chen Zhang <chenzhang2012@live.cn>
2014-10-10 02:38:51 -0700

    Update Longest Valid Parentheses.java

--- Longest Valid Parentheses.java
+++ Longest Valid Parentheses.java
@@ -1,21 +1,36 @@
+/*
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+*/
+
+// O(n), O(n) space
+
 public class Solution {
     public int longestValidParentheses(String s) {
-        if(s == null) return 0;
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
         // stack stores the index of s
         LinkedList<Integer> stack = new LinkedList<Integer>();
-        int start = 0, max = 0;
+        int max = 0;
+        // keep tracking start position of valid parentheses
+        int start = 0;
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(') {
+            char c = s.charAt(i);
+            if(c == '(') {
                 stack.push(i);
             } else {
                 if(stack.isEmpty()) {
+                    // invalid, move start position to the next
                     start = i + 1;
                 } else {
                     stack.pop();
-                    // )((())
-                    // stack top 1,2,3
-                    // !!! if stack.isEmpty(): max = (max, i - start + 1) !!!
-                    // !!! if not: max = i - stack.peek() !!!
+                    // after pop, if stack is empty, calculate the max = i - start + 1,
+                    // !!! if stack is not empty, max = i - stack.peek() !!!
+                    // because after pop and stack is still not empty, all chars after peek() must also be valid !
                     max = stack.isEmpty() ? Math.max(max, i - start + 1) : Math.max(max, i - stack.peek());
                 }
             }
