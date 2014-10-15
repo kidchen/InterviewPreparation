7531bf0500338f384231615564c5a86590402453
Chen Zhang <chenzhang2012@live.cn>
2014-10-15 00:10:12 -0700

    Update Sort List.java

--- Sort List.java
+++ Sort List.java
@@ -2,6 +2,8 @@
 Sort a linked list in O(n log n) time using constant space complexity.
 */
 
+// O(nlogn), O(logn) space for the recursion stack
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -19,7 +21,7 @@ public class Solution {
     }
     
     // seperate the list into two
-    ListNode seperate(ListNode head) {
+    private ListNode seperate(ListNode head) {
         if(head == null || head.next == null) {
             return head;
         }
@@ -32,31 +34,36 @@ public class Solution {
         ListNode head1 = head;
         ListNode head2 = slow.next;
         slow.next = null;
-        head1 = seperate(head1);
-        head2 = seperate(head2);
-        return merge(head1, head2);
+        // !!! do the recursion until there is only one element left, do the merge method so that we can get sorted lists !!!
+        return merge(helper(head1), helper(head2));
     }
     
     // merge two sorted lists
-    ListNode merge(ListNode head1, ListNode head2) {
-        ListNode result = new ListNode(0);
-        ListNode temp = result;
-        while(head1 != null && head2 != null) {
-            if(head1.val < head2.val) {
-                temp.next = head1;
-                head1 = head1.next;
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        }
+        if(l2 == null) {
+            return l1;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
             } else {
-                temp.next = head2;
-                head2 = head2.next;
+                cur.next = l2;
+                l2 = l2.next;
             }
-            temp = temp.next;
+            cur = cur.next;
         }
-        if(head1 != null) {
-            temp.next = head1;
+        if(l1 != null) {
+            cur.next = l1;
         }
-        if(head2 != null) {
-            temp.next = head2;
+        if(l2 != null) {
+            cur.next = l2;
         }
-        return result.next;
+        return dummy.next;
     }
 }
