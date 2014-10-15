c202f6b7c1c9654f31337b1247ea50364197411d
Chen Zhang <chenzhang2012@live.cn>
2014-10-15 05:14:24 -0700

    Update Restore IP Addresses.java

--- Restore IP Addresses.java
+++ Restore IP Addresses.java
@@ -7,7 +7,7 @@ Given "25525511135",
 return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
 */
 
-// Similar to NP, but only 4 layers of recursion
+// Similar to NP, but only 4 layers of recursion. O(n^4), O(2n) space
 
 public class Solution {
     public List<String> restoreIpAddresses(String s) {
