0b058e3437fb9340f6ed611a3158583400fa67b2
Chen Zhang <chenzhang2012@live.cn>
2014-10-15 00:44:28 -0700

    Update First Missing Positive.java

--- First Missing Positive.java
+++ First Missing Positive.java
@@ -1,20 +1,37 @@
+/*
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+*/
+
+// O(2n), O(1) space cost
+
 public class Solution {
     public int firstMissingPositive(int[] A) {
-        // only sort positive numbers, eg: [-1,4,1,9]-->[1,4,-1,9]
-        if(A == null || A.length == 0) return 1;
+        if(A == null || A.length == 0) {
+            return 1;
+        }
         for(int i = 0; i < A.length; i++) {
-            // A[A[i] - 1] != A[i] : consider [1,1]
+            // !!! A[A[i] - 1] vs. A[i] !!!
+            // we only consider numbers that are positive && smaller or equal to A.length
+            // !!! Also, we skip the numbers that already in the right position !!!
             if(A[i] <= A.length && A[i] > 0 && A[A[i] - 1] != A[i]) {
-                // temp should store A[A[i] - 1] rather than A[i], consider [2,1]
+                // !!! temp should store A[A[i] - 1] rather than A[i], consider [2,1] !!!
                 int temp = A[A[i] - 1];
                 A[A[i] - 1] = A[i];
                 A[i] = temp;
-                // !!! to still leave at this point !!!
+                // !!! to still leave at this point, or use a while loop !!!
                 i--;
             }
         }
         for(int i = 0; i < A.length; i++) {
-            if(A[i] != i + 1) return i + 1;
+            if(A[i] != i + 1) {
+                return i + 1;
+            }
         }
         return A.length + 1;
     }
