1c4a75eeb8223f44b0ddb2fdbd6cccbfd092309b
Chen Zhang <chenzhang2012@live.cn>
2014-10-16 04:03:04 -0700

    Update SearchforaRange.java

--- SearchforaRange.java
+++ SearchforaRange.java
@@ -1,50 +1,50 @@
+/*
+Given a sorted array of integers, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].
+*/
+
+// O(2logn), O(1) space
+
 public class Solution {
     public int[] searchRange(int[] A, int target) {
-        if(A.length==0 || A==null) return null;
         int[] result = {-1, -1};
-        int start = 0, end = A.length-1;
-        
-        // find the left min (start)
-        while(start<end){
-            if(start == end - 1){
-                if(A[start] != target){
-                    start = end;
-                }
-                break;
-            }
-            int mid = (start+end)/2;
-            if(A[mid] >= target){
-                end = mid;
-            }else{
-                start = mid;
-            }
-        }
-        
-        // if there is no match !!!
-        if(A[start]!=target){
+        if(A == null || A.length == 0) {
             return result;
         }
-        result[0] = start;
-        
-        // find the right max (end)
+        int start = 0;
+        int end = A.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(A[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        // !!! Binary Search Tip: if mid=target, move end, so that *start* will be in the first target !!!
+        int left = start;
         start = 0;
-        end = A.length-1;
-        while(start<end){
-            if(start == end - 1){
-                if(A[end] != target){
-                    end = start;
-                }
-                break;
+        end = A.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(A[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            int mid = (start+end)/2;
-            if(A[mid] > target){
-                end = mid;
-            }else{
-                start = mid;
-            }
-        }        
-        result[1] = end;
-        
+        }
+        // !!! careful of the condition: as long as left<=end, it is valid !!!
+        if(left <= end) {
+            result[0] = left;
+            result[1] = end;
+        }
         return result;
     }
 }
