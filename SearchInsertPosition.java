f62d721c2d7fb3fae5d66b15b48622ff8a8204e9
Chen Zhang <chenzhang2012@live.cn>
2014-10-15 23:58:13 -0700

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,5 +1,6 @@
 /*
-Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
 
 You may assume no duplicates in the array.
 
@@ -10,24 +11,26 @@ Here are few examples.
 [1,3,5,6], 0 → 0
 */
 
+// Binary search: O(logn), O(1) space
+
 public class Solution {
     public int searchInsert(int[] A, int target) {
         if(A == null || A.length == 0) {
             return 0;
         }
-        int left = 0;
-        int right = A.length - 1;
-        while(left <= right) {
-            int mid = (left + right) / 2;
+        int start = 0;
+        int end = A.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
             if(A[mid] == target) {
                 return mid;
             } else if(A[mid] < target) {
-                left = mid + 1;
+                start = mid + 1;
             } else {
-                right = mid - 1;
+                end = mid - 1;
             }
         }
-        // in case there is only one integer in A
-        return left;
+        // !!! tip: after the while loop, start/end will be in the position that larger/smaller then target !!!
+        return start;
     }
 }
