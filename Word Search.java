d60c0cf61bcc49025872b4c253dc4d08dba439a5
Chen Zhang <chenzhang2012@live.cn>
2014-10-15 17:00:41 -0700

    Update Word Search.java

--- Word Search.java
+++ Word Search.java
@@ -1,34 +1,55 @@
+/*
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+For example,
+Given board =
+
+[
+  ["ABCE"],
+  ["SFCS"],
+  ["ADEE"]
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
+*/
+
+// O(m^2 * n^2), O(1) space (or O(mn) space if we can't modify the original input)
+
 public class Solution {
     public boolean exist(char[][] board, String word) {
-        if(word == null || word.length() == 0) return true;
-        if(board == null || board.length == 0 || board[0].length == 0) return false;
-        boolean[][] used = new boolean[board.length][board[0].length];
+        if(board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        if(word == null || word.length() == 0) {
+            return true;
+        }
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
-            // if we can find a word:
-                if(search(board, word, used, i, j, 0)) {
-                    return true;
+                if(board[i][j] == word.charAt(0)) {
+                    if(helper(board, word, i, j, 0)) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
     
-    boolean search(char[][] board, String word, boolean[][] used, int i, int j, int index) {
-        // index: scan with the word
+    // or use an additional char[][] to store the used (if the original input is invalid to modify)
+    private boolean helper(char[][] board, String word, int row, int col, int index) {
         if(index == word.length()) {
             return true;
         }
-        // if i, j are legal, this board isn't used, and board == word, return true, otherwise return false
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || used[i][j] || board[i][j] != word.charAt(index)) {
+        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] == 'x' || board[row][col] != word.charAt(index)) {
             return false;
         }
-        // set this position as true(used)
-        used[i][j] = true;
-        // find its neighbor to match a char
-        boolean result = search(board, word, used, i + 1, j, index + 1) || search(board, word, used, i, j + 1, index + 1) || search(board, word, used, i - 1, j, index + 1) || search(board, word, used, i, j - 1, index + 1);
-        // if this position is existed, it will go further search, if not, set this position as false for future use
-        used[i][j] = false;
+        board[row][col] = 'x';
+        // if we can use the same char many times, we need add helper(board, word, row, col, index+1)
+        boolean result = helper(board, word, row + 1, col, index + 1) || helper(board, word, row - 1, col, index + 1) || helper(board, word, row, col + 1, index + 1) || helper(board, word, row, col - 1, index + 1);
+        board[row][col] = word.charAt(index);
         return result;
     }
 }
