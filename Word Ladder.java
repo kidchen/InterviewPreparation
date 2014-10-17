45e79b3845a6b2a1d4ce100096a527a240dda139
Chen Zhang <chenzhang2012@live.cn>
2014-10-16 17:35:21 -0700

    Update Word Ladder.java

--- Word Ladder.java
+++ Word Ladder.java
@@ -1,3 +1,72 @@
+/*
+Given two words (start and end), and a dictionary, 
+find the length of shortest transformation sequence from start to end, such that:
+
+Only one letter can be changed at a time
+Each intermediate word must exist in the dictionary
+For example,
+
+Given:
+start = "hit"
+end = "cog"
+dict = ["hot","dot","dog","lot","log"]
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+
+Note:
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+*/
+
+// O(min(26^L, size(dict)), O(min(26^L, size(dict)) space. L: length of the word
+
+public class Solution {
+    public int ladderLength(String start, String end, Set<String> dict) {
+        if(start == null || start.length() == 0 || end == null || end.length() == 0 || start.length() != end.length()) {
+            return 0;
+        }
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.offer(start);
+        // !!! have to remove !!!
+        dict.remove(start);
+        int result = 1;
+        while(!queue.isEmpty()) {
+            // !!! each time only change one char, we need to check all intermediate words in queue for THIS while loop !!!
+            // !!! have to use int outside for loop !!!
+            int poss = queue.size();
+            for(int i = 0; i < poss; i++) {
+                String cur = queue.poll();
+                for(int j = 0; j < cur.length(); j++) {
+                    char[] curChar = cur.toCharArray();
+                    // !!! for loop can use char as parameter !!!
+                    for(char c = 'a'; c <= 'z'; c++) {
+                        if(cur.charAt(j) == c) {
+                            continue;
+                        }
+                        curChar[j] = c;
+                        String temp = new String(curChar);
+                        if(temp.equals(end)) {
+                            return result + 1;
+                        }
+                        if(dict.contains(temp)) {
+                            queue.offer(temp);
+                            // don't forget to remove
+                            dict.remove(temp);
+                        }
+                    }
+                }
+            }
+            // !!! after loop all elements in queue, add 1 !!!
+            result++;
+        }
+        return 0;
+    }
+}
+
+
+/********* old version **********/
+
 public class Solution {
     public int ladderLength(String start, String end, Set<String> dict) {
         if(start == null || start.length() == 0 || end == null || end.length() == 0 || start.length() != end.length()) {
