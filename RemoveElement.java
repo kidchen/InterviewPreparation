b4bec03c596aed2fae062ef02fe8f1a176512944
Chen Zhang <chenzhang2012@live.cn>
2014-12-04 22:02:47 -0800

    Update RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -4,20 +4,22 @@ Given an array and a value, remove all instances of that value in place and retu
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 */
 
+// O(n) time, O(1) space
 
 public class Solution {
     public int removeElement(int[] A, int elem) {
-        int i=0, j=0;
-        while(j!=A.length){
-        // if the element is the one that will be removed, replace it by using its next element
-            if(A[j]!=elem){
-                A[i]=A[j];
-                // if not removing, both plus i, j
-                i++;
-            }
-            // otherwise, only plus j
-            j++;
+        if(A == null || A.length == 0) {
+            return 0;
         }
-        return i;
+        int cur = 0;
+        int len = 0;
+        while(cur < A.length) {
+            if(A[cur] != elem) {
+                A[len] = A[cur];
+                len++;
+            }
+            cur++;
+        }
+        return len;
     }
 }
