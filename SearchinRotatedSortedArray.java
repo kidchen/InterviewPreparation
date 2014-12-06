e286c404d819dc0e38cd8bbfea3f889dbb07fcf4
Chen Zhang <chenzhang2012@live.cn>
2014-12-06 12:40:20 -0800

    Update SearchinRotatedSortedArray.java

--- SearchinRotatedSortedArray.java
+++ SearchinRotatedSortedArray.java
@@ -1,3 +1,54 @@
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array
+*/
+
+// Binary search: O(logn) time, O(1) space
+
+public class Solution {
+    public int search(int[] A, int target) {
+        if(A == null || A.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = A.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(A[mid] == target) {
+                return mid;
+            } else if(A[start] < A[mid]) {
+                // left is sorted
+                if(target >= A[start] && target < A[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if(A[start] > A[mid]) {
+                // right is sorted
+                if(target > A[mid] && target <= A[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                start++;
+            }
+        }
+        return -1;
+    }
+}
+
+
+// Not easy to think!
+// conditions: A[start]<A[end]: 1,2,[3],4  normal situation
+//             A[end]>=target: 3,4,[1],2  right part is increasing
+//             A[mid]>A[start]: 2,3,[4],1  right part is not increasing
+
 public class Solution {
     public int search(int[] A, int target) {
         int start=0, end=A.length-1;
@@ -16,48 +67,6 @@ public class Solution {
                     start=mid+1;
                 }else{
                     end=mid-1;
-                }
-            }
-        }
-        return -1;
-    }
-}
-
-// Not easy to think!
-
-// conditions: A[start]<A[end]: 1,2,[3],4  normal situation
-//             A[end]>=target: 3,4,[1],2  right part is increasing
-//             A[mid]>A[start]: 2,3,[4],1  right part is not increasing
-
-
-// More reasonable (easier to think) solution:
-
-public class Solution {
-    public int search(int[] A, int target) {
-        if(A == null || A.length == 0) {
-            return -1;
-        }
-        int start = 0;
-        int end = A.length - 1;
-        while(start <= end) {
-            int mid = (start + end) / 2;
-            if(A[mid] == target) {
-                return mid;
-            }
-            // if right part is in ordered
-            if(A[mid] < A[end]) {
-                // check if target is in the right part
-                // !!! careful at ">=" since boundary condition is not checked !!!
-                if(A[mid] < target && A[end] >= target) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            } else {
-                if(A[start] <= target && A[mid] > target) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
                 }
             }
         }
