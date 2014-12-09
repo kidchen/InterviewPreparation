bec932d6d005b2b08a7d797d2e722f63ca70edb1
Chen Zhang <chenzhang2012@live.cn>
2014-12-09 13:00:47 -0800

    Update Insert node into BST.java

--- Insert node into BST.java
+++ Insert node into BST.java
@@ -1,47 +1,96 @@
+/*
+Given a binary search tree  and a new tree node, insert the node into the tree. 
+You should keep the tree still be a valid binary search tree.
+
+Example
+Given binary search tree as follow:
+    2
+  /   \
+1      4
+      /   
+     3 
+after Insert node 6, the tree should be:
+    2
+  /    \
+1       4
+      /   \ 
+     3     6
+*/
+
 // recursion
 
-public void insertNode(int key) {
-    root = insertNode(root, new Node(key));
-}
-//private recursive call
-private Node insertNode(Node currentParent, Node newNode) {
-    if (currentParent == null) 
-        return newNode;
-    else if (newNode.val > currentParent.val)
-        currentParent.right = insertNode(currentParent.right, newNode);
-    else if (newNode.val < currentParent.val)
-        currentParent.left = insertNode(currentParent.left, newNode);
-    return currentParent; 
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        if(root == null) {
+            return node;
+        }
+        if(root.val > node.val) {
+            root.left = insertNode(root.left, node);
+        } else {
+            root.right = insertNode(root.right, node);
+        }
+        return root;
+    }
 }
 
 
 // iterative
 
-public void insertNode(Node root, int key) { 
-    Node newNode = new Node(key);
-    if (root == null) {
-        root = newNode;
-        return; 
-    }
-    Node parent = null; 
-    Node cur = root; 
-    while (cur != null) {
-        parent = cur; 
-        if(newNode.val < cur.val) {
-            cur = cur.left;
-        } else if (newNode.val > cur.val) {
-            cur = cur.right; 
-        } else {
-        // overwrite duplicate 
-            cur = newNode; 
-            return;
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        if(root == null) {
+            root = node;
+            return root;
         }
+        TreeNode parent = null;
+        TreeNode cur = root;
+        while(cur != null) {
+            parent = cur;
+            if(cur.val > node.val) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        if(node.val < parent.val) {
+            parent.left = node;
+        } else {
+            parent.right = node;
+        }
+        return root;
     }
-    int res = key.compareTo(parent.key); 
-    if (key < parent.val) {
-        parent.left = newNode; 
-    } else {
-        parent.right = newNode; 
-    }
-    return; 
 }
