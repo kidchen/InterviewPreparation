899fd9f2c1376924a9e5ee169e414326286421ac
Chen Zhang <chenzhang2012@live.cn>
2014-12-10 13:56:44 -0800

    Update PartitionList.java

--- PartitionList.java
+++ PartitionList.java
@@ -11,6 +11,57 @@ return 1->2->2->4->3->5.
 // O(n), O(1) space
 
 /**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param x: an integer
+     * @return: a ListNode 
+     */
+    public ListNode partition(ListNode head, int x) {
+        // write your code here
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = dummy;
+        while(cur.next != null) {
+            if(cur.next.val < x) {
+                // !!! have to check if cur == pre !!!
+                if(cur != pre) {
+                    ListNode temp = cur.next;
+                    cur.next = cur.next.next;
+                    ListNode last = pre.next;
+                    pre.next = temp;
+                    pre = pre.next;
+                    pre.next = last;
+                } else {
+                    pre = pre.next;
+                    cur = cur.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
+
+
+// LeetCode:
+
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
