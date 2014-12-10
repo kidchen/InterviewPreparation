9f6902f6ea8383359b9f874f3ef9f6d671aa1bd8
Chen Zhang <chenzhang2012@live.cn>
2014-12-09 18:29:41 -0800

    Update Jump Game II.java

--- Jump Game II.java
+++ Jump Game II.java
@@ -13,26 +13,20 @@ The minimum number of jumps to reach the last index is 2. (Jump 1 step from inde
 
 // DP(local & global) : O(n), O(1)
 
-
 public class Solution {
     public int jump(int[] A) {
         if(A == null || A.length == 0) {
             return 0;
         }
-        /*
-        maxPath: so far, the max path(index) we can reach
-        lastMaxPath: when current path(index) is bigger than lastmaxPath, step++, set the lastMaxPath as maxPath
-        step: calculate how many steps should we take
-        */
-        int maxPath = 0, lastMaxPath = 0, step = 0;
+        // lastPath: last step within maxPath, if i>lastPath, 
+        // that means we found a step within maxPath which can reach further, so we step++
+        int maxPath = 0, lastPath = 0, step = 0;
         // !!! add condition " i <= maxPath", so that we can break when there is unreachable to the last !!!
         for(int i = 0; i < A.length && i <= maxPath; i++) {
-            // current position is beyond the last jump: add step and jump to the next maxPath
-            if(i > lastMaxPath) {
+            if(i > lastPath) {
                 step++;
-                lastMaxPath = maxPath;
+                lastPath = maxPath;
             }
-            // calculate the maxPath in current position
             maxPath = Math.max(A[i] + i, maxPath);
         }
         // check the availability
