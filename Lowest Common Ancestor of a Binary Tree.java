25d4e39a26afb834e5c672334ae174523d5d647f
Chen Zhang <chenzhang2012@live.cn>
2014-12-12 17:34:51 -0800

    Update Lowest Common Ancestor of a Binary Tree.java

--- Lowest Common Ancestor of a Binary Tree.java
+++ Lowest Common Ancestor of a Binary Tree.java
@@ -1,24 +1,58 @@
 /*
-Given a binary tree, find the lowest common ancestor of two given nodes in the tree.
+Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
+
+The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+
+Example
+     4
+    / \
+  3    7
+     /   \
+    5     6
+
+For 3 and 5, the LCA is 4.
+For 5 and 6, the LCA is 7.
+For 6 and 7, the LCA is 7
 */
 
 // O(n), O(logn) space for recursion
 
-    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-        
-        if (root == p || root == q) return root;
-        
-        // Post order traveral
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        
-        if (left != null && right != null) {
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param A and B: two nodes in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+        // write your code here
+        if(root == null) {
+            return null;
+        }
+        if(root == A || root == B) {
+            return root;
+        }
+        // postorder
+        TreeNode left = lowestCommonAncestor(root.left, A, B);
+        TreeNode right = lowestCommonAncestor(root.right, A, B);
+        if(left != null && right != null) {
             return root;
         } else {
-            return left != null ? left : right;
+            return left == null ? right : left;
         }
     }
+}
+
     
     
 // follow up:
