45549f1d6c18d04aa4d85c95e51eab3c31935f13
Chen Zhang <chenzhang2012@live.cn>
2014-12-13 17:22:20 -0800

    Update Insert Interval.java

--- Insert Interval.java
+++ Insert Interval.java
@@ -1,3 +1,20 @@
+/*
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+*/
+
+// O(n) time/space
+// if we store as linked list, we can do it in-place so that the space cost O(1)
+
 /**
  * Definition for an interval.
  * public class Interval {
@@ -8,28 +25,31 @@
  * }
  */
 public class Solution {
-    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
-        // [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9]
-        ArrayList<Interval> result = new ArrayList<Interval>();
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> result = new ArrayList<Interval>();
         if(intervals == null || intervals.size() == 0) {
             result.add(newInterval);
             return result;
         }
         int i = 0;
-        // [1,2],[3,4]... insert[2,4] ==> don't add '='
+        // add all before newInterval
         while(i < intervals.size() && intervals.get(i).end < newInterval.start) {
             result.add(intervals.get(i));
             i++;
         }
+        // change newInterval.start if: [4,5] insert into [1,2][3,6] --> [4,5]->[3,5]
         if(i < intervals.size()) {
-            newInterval.start = Math.min(intervals.get(i).start, newInterval.start);
+            newInterval.start = Math.min(newInterval.start, intervals.get(i).start);
         }
+        // add newInterval
         result.add(newInterval);
-        // [1,5] insert [0,1] ==> add '='
+        // change newInterval.end
+        // !!! attention to "=" !!!
         while(i < intervals.size() && intervals.get(i).start <= newInterval.end) {
             result.get(result.size() - 1).end = Math.max(intervals.get(i).end, newInterval.end);
             i++;
         }
+        // add all after newInterval
         while(i < intervals.size()) {
             result.add(intervals.get(i));
             i++;
@@ -37,3 +57,11 @@ public class Solution {
         return result;
     }
 }
+
+
+// follow up:
+// If when insert, it should do merge, we return without insert.
+
+/*
+Do it by binary search: We do insert if no need to merge. Otherwise we do binary search and reduce the time cost to O(logn).
+*/
