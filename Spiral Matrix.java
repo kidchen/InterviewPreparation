918e0df113c4e55b0a10b4cb99d5fb2e844db902
Chen Zhang <chenzhang2012@live.cn>
2014-12-14 19:38:50 -0800

    Update Spiral Matrix.java

--- Spiral Matrix.java
+++ Spiral Matrix.java
@@ -1,10 +1,24 @@
+/*
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+For example,
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
+*/
+
+// O(m*n) time, O(1) space
+
 public class Solution {
     public ArrayList<Integer> spiralOrder(int[][] matrix) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return result;
         // level by level
-        // levelNum: how many levels is there in the matrix
-        // !!! min is useful later !!!
         int min = Math.min(matrix.length, matrix[0].length);
         int levelNum = min / 2;
         for(int level = 0; level < levelNum; level++) {
@@ -26,10 +40,10 @@ public class Solution {
             }
         }
         // for the last one col or row
-        // !!! have to use min to check whether there is one line left !!!
         if(min % 2 == 1) {
             // row < col
             if(matrix[0].length > matrix.length) {
+                // !!! here, i<length - levelNum, no need to -1 !!!
                 for(int i = levelNum; i < matrix[0].length - levelNum; i++) {
                     result.add(matrix[levelNum][i]);
                 }                
