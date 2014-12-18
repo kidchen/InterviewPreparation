3cb58e692f925e2bca3081d7ac18830ffffab0fb
Chen Zhang <chenzhang2012@live.cn>
2014-12-17 23:01:13 -0800

    Update Sort List.java

--- Sort List.java
+++ Sort List.java
@@ -5,23 +5,31 @@ Sort a linked list in O(n log n) time using constant space complexity.
 // O(nlogn), O(logn) space for the recursion stack
 
 /**
- * Definition for singly-linked list.
- * class ListNode {
+ * Definition for ListNode.
+ * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
  *     }
  * }
- */
+ */ 
 public class Solution {
-    public ListNode sortList(ListNode head) {
+    /**
+     * @param head: The head of linked list.
+     * @return: You should return the head of the sorted linked list,
+                    using constant space complexity.
+     */
+    public ListNode sortList(ListNode head) {  
+        // write your code here
+        if(head == null || head.next == null) {
+            return head;
+        }
         return seperate(head);
     }
     
-    // seperate the list into two
-    private ListNode seperate(ListNode head) {
+    private ListNode separate(ListNode head) {
         if(head == null || head.next == null) {
             return head;
         }
@@ -35,34 +43,27 @@ public class Solution {
         ListNode head2 = slow.next;
         slow.next = null;
         // !!! do the recursion until there is only one element left, do the merge method so that we can get sorted lists !!!
-        return merge(helper(head1), helper(head2));
+        return merge(separate(head1), separate(head2));
     }
     
-    // merge two sorted lists
-    private ListNode merge(ListNode l1, ListNode l2) {
-        if(l1 == null) {
-            return l2;
-        }
-        if(l2 == null) {
-            return l1;
-        }
+    private ListNode merge(ListNode head1, ListNode head2) {
         ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
+        ListNode pre = dummy;
+        while(head1 != null && head2 != null) {
+            if(head1.val < head2.val) {
+                pre.next = head1;
+                head1 = head1.next;
             } else {
-                cur.next = l2;
-                l2 = l2.next;
+                pre.next = head2;
+                head2 = head2.next;
             }
-            cur = cur.next;
+            pre = pre.next;
         }
-        if(l1 != null) {
-            cur.next = l1;
+        if(head1 != null) {
+            pre.next = head1;
         }
-        if(l2 != null) {
-            cur.next = l2;
+        if(head2 != null) {
+            pre.next = head2;
         }
         return dummy.next;
     }
