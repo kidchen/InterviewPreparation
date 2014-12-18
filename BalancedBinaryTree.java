25402867d4a58bc6fc57005b195576003f09b839
Chen Zhang <chenzhang2012@live.cn>
2014-12-17 17:51:31 -0800

    Update BalancedBinaryTree.java

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -1,58 +1,60 @@
+/*
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which 
+the depth of the two subtrees of every node never differ by more than 1.
+
+Example
+Given binary tree A={3,9,20,#,#,15,7}, B={3,#,20,15,7}
+
+A)  3            B)    3 
+   / \                  \
+  9  20                 20
+    /  \                / \
+   15   7              15  7
+The binary tree A is a height-balanced binary tree, but B is not.
+*/
+
+// O(n) time, O(logn) space
+
 /**
- * Definition for binary tree
+ * Definition of TreeNode:
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
  * }
  */
 public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        if(checkHeight(root)==-1){
+        // write your code here
+        if(root == null) {
+            return true;
+        }
+        if(helper(root) == -1) {
             return false;
         }
         return true;
     }
     
-    public int checkHeight(TreeNode root){
-        if(root==null) return 0;
-        int leftHeight = checkHeight(root.left);
-        int rightHeight = checkHeight(root.right);
-        if(leftHeight == -1 || rightHeight == -1) return -1;
-        if(Math.abs(leftHeight - rightHeight) > 1) return -1;
-        return Math.max(leftHeight, rightHeight) + 1;
-    }
-}
-
-
-// DFS. O(n), O(logn), recursion:
-
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public boolean isBalanced(TreeNode root) {
-        return helper(root) >= 0;
-    }
-    
-    int helper(TreeNode root) {
+    private int helper(TreeNode root) {
         if(root == null) {
             return 0;
         }
         int left = helper(root.left);
         int right = helper(root.right);
-        if(left < 0 || right < 0) {
+        // !!! if one of the subtree is not balanced, return -1 !!!
+        if(left == -1 || right == -1) {
             return -1;
         }
-        if(Math.abs(left - right) >= 2) {
+        if(Math.abs(left - right) > 1) {
             return -1;
         }
         // !!! return the MAX one + 1 as the depth !!!
