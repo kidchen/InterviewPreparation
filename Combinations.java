c8b4dd9e4af9234bd83ff275216b3cdcd6cf98a9
Chen Zhang <chenzhang2012@live.cn>
2014-12-22 15:38:26 -0800

    Update Combinations.java

--- Combinations.java
+++ Combinations.java
@@ -14,27 +14,34 @@ If n = 4 and k = 2, a solution is:
 ]
 */
 
+// np, O(n^k) time
+
 public class Solution {
+    /**
+     * @param n: Given the range of numbers
+     * @param k: Given the numbers of combinations
+     * @return: All the combinations of k numbers out of 1..n
+     */
     public ArrayList<ArrayList<Integer>> combine(int n, int k) {
-        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> eachResult = new ArrayList<Integer>();
-        if(n<1 || k<1) return results;
-        helper(1,n,k,eachResult,results);
-        return results;
+		// write your code here
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> items = new ArrayList<Integer>();
+		if(n < 1 || k < 1) {
+		    return result;
+		}
+		helper(n, k, result, items, 1);
+		return result;
     }
     
-    void helper(int start, int n, int k, ArrayList<Integer> eachResult, ArrayList<ArrayList<Integer>> results){
-        if(eachResult.size()==k){
-            // !!! have to use a new ArrayList
-            // !!! eachResult will be removed later
-            results.add(new ArrayList<Integer>(eachResult)); 
+    private void helper(int n, int k, ArrayList<ArrayList<Integer>> result, ArrayList<Integer> items, int start) {
+        if(items.size() == k) {
+            result.add(new ArrayList<Integer>(items));
             return;
-        }else{
-            for(int i=start; i<=n; i++){
-                eachResult.add(i);
-                helper(i+1,n,k,eachResult,results);
-                eachResult.remove(eachResult.size()-1);
-            }
+        }
+        for(int i = start; i <= n; i++) {
+            items.add(i);
+            helper(n, k, result, items, i + 1);
+            items.remove(items.size() - 1);
         }
     }
 }
