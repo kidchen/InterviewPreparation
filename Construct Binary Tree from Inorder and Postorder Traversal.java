38910a8d685bdd7d221e0e81fef0e30fc4c39882
Chen Zhang <chenzhang2012@live.cn>
2014-12-23 18:49:21 -0800

    Update Construct Binary Tree from Inorder and Postorder Traversal.java

--- Construct Binary Tree from Inorder and Postorder Traversal.java
+++ Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -45,3 +45,50 @@ public class Solution {
         return root;
     }
 }
+
+
+// LintCode:
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+ 
+public class Solution {
+    /**
+     *@param inorder : A list of integers that inorder traversal of a tree
+     *@param postorder : A list of integers that postorder traversal of a tree
+     *@return : Root of a tree
+     */
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        // write your code here
+        int inEnd = inorder.length - 1;
+        int postEnd = postorder.length - 1;
+        return helper(inorder, postorder, 0, inEnd, 0, postEnd);
+    }
+    
+    private TreeNode helper(int[] inorder, int[] postorder, int inStart, int inEnd, int postStart, int postEnd) {
+        if(inStart > inEnd || postStart > postEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postEnd]);
+        int leftLen = 0;
+        for(int i = inStart; i <= inEnd; i++) {
+            if(inorder[i] == postorder[postEnd]) {
+                break;
+            }
+            leftLen++;
+        }
+        root.left = helper(inorder, postorder, inStart, inStart + leftLen - 1, postStart, postStart + leftLen - 1);
+        root.right = helper(inorder, postorder, inStart + leftLen + 1, inEnd, postStart + leftLen, postEnd - 1);
+        return root;
+    }
+}
