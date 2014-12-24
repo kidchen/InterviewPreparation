802fd7b6d73afa2406e2f8c6a7f667d5d8779e2a
Chen Zhang <chenzhang2012@live.cn>
2014-12-23 17:40:44 -0800

    Update Construct Binary Tree from Preorder and Inorder Traversal.java

--- Construct Binary Tree from Preorder and Inorder Traversal.java
+++ Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -43,3 +43,52 @@ public class Solution {
         return root;
     }
 }
+
+
+// LintCode:
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    /**
+     *@param preorder : A list of integers that preorder traversal of a tree
+     *@param inorder : A list of integers that inorder traversal of a tree
+     *@return : Root of a tree
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // write your code here
+        if(preorder == null && inorder == null) {
+            return null;
+        }
+        int preEnd = preorder.length - 1;
+        int inEnd = inorder.length - 1;
+        return helper(preorder, inorder, 0, preEnd, 0, inEnd);
+    }
+    
+    private TreeNode helper(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
+        if(preStart > preEnd || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int leftLen = 0;
+        for(int i = inStart; i <= inEnd; i++) {
+            if(inorder[i] == preorder[preStart]) {
+                break;
+            }
+            leftLen++;
+        }
+        root.left = helper(preorder, inorder, preStart + 1, preStart + leftLen, inStart, inStart + leftLen);
+        root.right = helper(preorder, inorder, preStart + leftLen + 1, preEnd, inStart + leftLen + 1, inEnd);
+        return root;
+    }
+}
