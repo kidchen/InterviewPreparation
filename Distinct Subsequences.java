c5016dfbc74ab426563a2dfa34a80249ea034982
Chen Zhang <chenzhang2012@live.cn>
2014-12-24 17:55:29 -0800

    Update Distinct Subsequences.java

--- Distinct Subsequences.java
+++ Distinct Subsequences.java
@@ -1,5 +1,17 @@
-public class Solution {
-    public int numDistinct(String S, String T) {
+/*
+Given a string S and a string T, count the number of distinct subsequences of T in S.
+
+A subsequence of a string is a new string which is formed from the original string 
+by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+(ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+Example
+S = "rabbbit", T = "rabbit"
+
+Return 3.
+*/
+
+// DP: O(m*n) time/space
 /*
   * r a b b b i t (s)
 * 1 1 1 1 1 1 1 1
@@ -13,23 +25,66 @@ t 0 0 0 0 0 0 0 3
 *: null
 http://blog.csdn.net/abcbc/article/details/8978146
 */
-        // need additional +1 to store the first row & col for initial
-        int[][] dp = new int[T.length() + 1][S.length() + 1];
-        for(int i = 0; i < S.length() + 1; i++) {
-            dp[0][i] = 1;
+
+public class Solution {
+    /**
+     * @param S, T: Two string.
+     * @return: Count the number of distinct subsequences
+     */
+    public int numDistinct(String S, String T) {
+        // write your code here
+        if(T.length() == 0) {
+            return 1;
         }
-        for(int j = 1; j < T.length() + 1; j++) {
-            dp[j][0] = 0;
+        if(S.length() == 0) {
+            return 0;
         }
-        for(int i = 1; i < T.length() + 1; i++) {
-            for(int j = 1; j < S.length() + 1; j++) {
-                dp[i][j] = dp[i][j - 1];
-                // !!! Do not forget to -1 when get the index !!!
-                if(T.charAt(i - 1) == S.charAt(j - 1)) {
-                    dp[i][j] += dp[i - 1][j - 1];
+        int[][] result = new int[T.length() + 1][ S.length() + 1];
+        // initial the first row as 1
+        for(int i = 0; i <= S.length(); i++) {
+            result[0][i] = 1;
+        }
+        for(int i = 1; i <= T.length(); i++) {
+            for(int j = 1; j <= S.length(); j++) {
+                if(S.charAt(j - 1) == T.charAt(i - 1)) {
+                    // match: left + top-left
+                    result[i][j] = result[i - 1][j - 1] + result[i][j - 1];
+                } else {
+                    // not match: same as the left
+                    result[i][j] = result[i][j - 1];
                 }
             }
         }
-        return dp[T.length()][S.length()];
+        return result[T.length()][S.length()];
+    }
+}
+
+
+// O(m*n) time, O(m) space
+
+public class Solution {
+    /**
+     * @param S, T: Two string.
+     * @return: Count the number of distinct subsequences
+     */
+    public int numDistinct(String S, String T) {
+        // write your code here
+        if(T.length() == 0) {
+            return 1;
+        }
+        if(S.length() == 0) {
+            return 0;
+        }
+        int[] result = new int[T.length() + 1];
+        result[0] = 1;
+        for(int i = 0; i < S.length(); i++) {
+            // !!! have to do it from bottom to top, so that the result[j - 1] will keep the original !!!
+            for(int j = T.length(); j > 0; j--) {
+                if(S.charAt(i) == T.charAt(j - 1)) {
+                    result[j] += result[j - 1];
+                } 
+            }
+        }
+        return result[T.length()];
     }
 }
