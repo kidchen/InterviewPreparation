99d51a4920be5e898072126c7d375059b546807a
Chen Zhang <chenzhang2012@live.cn>
2014-12-28 21:55:09 -0800

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -1,3 +1,59 @@
+/*
+Given a list of numbers, return all possible permutations.
+
+Example
+For nums [1,2,3], the permutaions are: 
+
+[
+    [1,2,3],
+    [1,3,2],
+    [2,1,3],
+    [2,3,1],
+    [3,1,2],
+    [3,2,1]
+]
+*/
+
+// NP: O(2^n)
+// recursion: use extra space to store the used elements
+
+class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> nums) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(nums == null || nums.size() == 0) {
+            return result;
+        }
+        ArrayList<Integer> item = new ArrayList<Integer>();
+        boolean[] used = new boolean[nums.size()];
+        helper(nums, result, item, used);
+        return result;
+    }
+    
+    private void helper(ArrayList<Integer> nums, ArrayList<ArrayList<Integer>> result, ArrayList<Integer> item, boolean[] used) {
+        if(item.size() == nums.size()) {
+            result.add(new ArrayList<Integer>(item));
+            return;
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(!used[i]) {
+                used[i] = true;
+                item.add(nums.get(i));
+                helper(nums, result, item, used);
+                item.remove(item.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
+}
+
+
+// iteration: got 0-i elements' permutation, when we got i+1th, add the new one to each previous items
+
 public class Solution {
     public ArrayList<ArrayList<Integer>> permute(int[] num) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
