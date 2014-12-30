f05fc9e4ebfb817c2216d3b254c6401cf2598ee9
Chen Zhang <chenzhang2012@live.cn>
2014-12-29 17:38:16 -0800

    Update Reverse Words in a String.java

--- Reverse Words in a String.java
+++ Reverse Words in a String.java
@@ -1,21 +1,50 @@
+/*
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Example
+Clarification
+What constitutes a word?
+A sequence of non-space characters constitutes a word.
+Could the input string contain leading or trailing spaces?
+Yes. However, your reversed string should not contain leading or trailing spaces.
+How about multiple spaces between two words?
+Reduce them to a single space in the reversed string.
+*/
+
+// O(n) time/space
+
 public class Solution {
+    /**
+     * @param s : A string
+     * @return : A string
+     */
     public String reverseWords(String s) {
-        if(s == null || s.length() == 0) return s;
+        // write your code
+        if(s == null || s.length() == 0) {
+            return s;
+        }
         s = s.trim();
-        int start = 0, end = s.length() - 1;
-        StringBuilder result = new StringBuilder();
-        while(start <= end) {
-            StringBuilder word = new StringBuilder();
-            while(start <= end && s.charAt(end) != ' ') {
-                word.append(s.charAt(end));
-                end--;
+        StringBuffer result = new StringBuffer();
+        int i = s.length() - 1;
+        while(i >= 0) {
+            StringBuffer word = new StringBuffer();
+            while(i >= 0 && s.charAt(i) != ' ') {
+                word.append(s.charAt(i));
+                i--;
             }
             word.reverse();
-            result.append(word + " ");
-            while(start <= end && s.charAt(end) == ' ') {
-                end--;
+            result.append(word);
+            while(i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            if(i >= 0) {
+                result.append(' ');
             }
         }
-        return result.toString().trim();
+        return result.toString();
     }
 }
