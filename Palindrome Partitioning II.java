cf6e13d3cf4f19416d072bccd6ec1cd1fdcf766c
Chen Zhang <chenzhang2012@live.cn>
2015-01-10 18:56:08 -0800

    Update Palindrome Partitioning II.java

--- Palindrome Partitioning II.java
+++ Palindrome Partitioning II.java
@@ -1,3 +1,45 @@
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+*/
+
+// O(n^2) time cost, with O(n) space
+
+public class Solution {
+	public int minCut(String s) {
+		int n = s.length();
+		boolean dict[][] = new boolean[n][n];
+		int cut[] = new int[n];
+		// j: the end of the cur substring
+		for (int j = 0; j < n; j++) {
+		    // as each char is a palindrome
+			cut[j] = j;
+			// i: the start of the cur substring
+			for (int i = 0; i <= j; i++) {
+			    // if s.start == s.end && (s is a char || between i and j is also palindrome)
+				if (s.charAt(i) == s.charAt(j) && (j - i <= 1 || dict[i + 1][j - 1])) {
+					dict[i][j] = true;
+					if (i > 0) {
+					    // dp: [i,j] is palindrome, check whether prevSum + cur is smaller
+					    cut[j] = Math.min(cut[j], cut[i - 1] + 1);
+					} else {
+					    // if i == 0(this is a whole palindrome)
+					    cut[j] = 0; 
+					}
+				}
+			}
+		}
+		// return when j = n - 1
+		return cut[n - 1];
+	}
+}
+
+
+/****** old version ******/
 public class Solution {
     public int minCut(String s) {
         if(s == null || s.length() == 0) return 0;
