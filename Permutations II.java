3a8044e390cdcf0a2390f35c7209c8b5a3638025
Chen Zhang <chenzhang2012@live.cn>
2015-01-10 18:19:27 -0800

    Update Permutations II.java

--- Permutations II.java
+++ Permutations II.java
@@ -1,3 +1,13 @@
+/*
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[1,1,2], [1,2,1], and [2,1,1].
+*/
+
+// 
+
 public class Solution {
     public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
@@ -22,6 +32,7 @@ public class Solution {
                 helper(num, set, visited, result);
                 visited[i] = false;
                 set.remove(set.size() - 1);
+                // check dup in the if() clause
                 while(i+1 < num.length && num[i] == num[i+1]){
                     i++;
                 }
