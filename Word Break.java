d6a020a4f9d504b33bed6b4f318d8f0a524374d9
Chen Zhang <chenzhang2012@live.cn>
2015-01-25 18:46:01 -0800

    Update Word Break.java

--- Word Break.java
+++ Word Break.java
@@ -9,6 +9,51 @@ dict = ["leet", "code"].
 Return true because "leetcode" can be segmented as "leet code".
 */
 
+// DP: O(nl) time, here n is the length of the string and l is the max length of the words in dict (O()
+
+public class Solution {
+    /**
+     * @param s: A string s
+     * @param dict: A dictionary of words dict
+     */
+    public boolean wordSegmentation(String s, Set<String> dict) {
+        // write your code here 
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        int maxLength = maxLength(dict);
+        boolean[] canSeg = new boolean[s.length() + 1];
+        canSeg[0] = true;
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = 1; j <= i && j <= maxLength; j++) {
+                // [i - j], so that we can find the position which last time all the substrings can be found in dict
+                // think in this way: for the initialization, only canSag[0] is true, but j starts from 1, so we have to do i - j
+                if(!canSeg[i - j]) {
+                    continue;
+                }
+                // !!! [i - j] !!!
+                String word = s.substring(i - j, i);
+                if(dict.contains(word)) {
+                    canSeg[i] = true;
+                    break;
+                }
+            }
+        }
+        return canSeg[s.length()];
+    }
+    
+    // get the max length of the word in dict
+    private int maxLength(Set<String> dict) {
+        int maxLength = 0;
+        for(String word : dict) {
+            maxLength = Math.max(maxLength, word.length());
+        }
+        return maxLength;
+    }
+}
+
+
+/********** old version **********/
 // DP: O(n^2), O(n)
 
 public class Solution {
