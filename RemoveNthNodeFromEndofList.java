7487a9f5ab028828bc6eea054f48b1a17b1e7cc3
Chen Zhang <chenzhang2012@live.cn>
2015-02-02 00:47:32 -0800

    Update RemoveNthNodeFromEndofList.java

--- RemoveNthNodeFromEndofList.java
+++ RemoveNthNodeFromEndofList.java
@@ -2,16 +2,62 @@
 Given a linked list, remove the nth node from the end of list and return its head.
 
 For example,
-
    Given linked list: 1->2->3->4->5, and n = 2.
-
    After removing the second node from the end, the linked list becomes 1->2->3->5.
+
 Note:
 Given n will always be valid.
+
 Try to do this in one pass.
 */
 
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer.
+     * @return: The head of linked list.
+     */
+    ListNode removeNthFromEnd(ListNode head, int n) {
+        // write your code here
+        if(n == 0) {
+            return head;
+        }
+        if(head == null || head.next == null) {
+            return null;
+        }
+        int count = 0;
+        ListNode end = head;
+        while(count < n) {
+            end = end.next;
+            count++;
+        }
+        ListNode node = head;
+        if(end == null) {
+            return head.next;
+        } else {
+            while(end.next != null) {
+                node = node.next;
+                end = end.next;
+            }
+            node.next = node.next.next;
+        }
+        return head;
+    }
+}
 
+
+/*********** Old Version *************/
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -40,51 +86,6 @@ public class Solution {
             slow=slow.next;
         }
         slow.next=slow.next.next;
-        return head;
-    }
-}
-
-/***********UPDATE AUG 16*************/
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head == null) {
-            return head;
-        }
-        ListNode fast = head;
-        int i = 0;
-        // !!! fast, not fast.next != null !!!
-        // keep in mind that if only one element in the list
-        while(fast != null && i < n) {
-            fast = fast.next;
-            i++;
-        }
-        // if n is invalid (bigger than the number of list nodes)
-        if(i < n) {
-            return head;
-        }
-        // !!! if n equals to the number of list nodes (that is, we remove the head) !!!
-        if(fast == null) {
-            return head.next;
-        }
-        // general cases
-        ListNode slow = head;
-        while(fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        slow.next = slow.next.next;
         return head;
     }
 }
